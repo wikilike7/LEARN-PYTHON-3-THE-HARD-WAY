031bc902f9b93e7d5a5ceaeef7fe255c8907360c
wikilike7 <wikilike7@gmail.com>
2018-10-17 11:02:48 +0800

    updates

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by Python
+
+print('I could have code like this.') # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment our code:
+# print('This wan\'t run.')
+
+print('This will run.')
+
---
+++ ex3.py
@@ -0,0 +1,26 @@
+print('I will not count my chickens:')
+
+print('Hens', 25 + 30 /6)
+print('Roosters', 100 - 25 * 3 % 4)
+
+print('Now i will count the eggs:')
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print('Is it true that 3 + 2 < 5 - 7?')
+
+# calculate 3 plus 2 compare to 5 minus 7
+print(3 + 2 < 5 - 7)
+
+print('What is 3 + 2?', 3 + 2)
+print('What is 5 - 7', 5 - 7)
+
+print('Oh, that\'s what it\'s False.')
+
+print('How about some more.')
+
+print('Is it greater?', 5 > -2)
+print('Is it greater or equal?', 5 >= -2)
+print('Is it less or equal?', 5 <= -2)
+
+print('5.0 plus 2.0 equal to', 5.0 + 2.0)
