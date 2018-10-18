e5b88d629abb7306fa77174c43b7b913d0b654fa
wikilike7 <wikilike7@gmail.com>
2018-10-18 17:24:53 +0800

    updates

--- ex5.py
+++ ex5.py
@@ -8,7 +8,7 @@ hair = 'Brown'
 
 print(f"Let's talk about {name}.")
 print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavey.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
