1a58e221cea67e877c5e996d5b1860689f04f66c
wikilike7 <wikilike7@gmail.com>
2018-10-18 15:33:58 +0800

    updates

---
+++ ex7.py
@@ -0,0 +1,10 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print('And everywhere that Mary went.')
+print("." * 10)  # what'd that do?
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+
+print(end1 + end2 + end3, end='')
